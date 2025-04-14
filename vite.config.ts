343b3390d551f19a9d74ecfa554b46777d5782f0
juliannGabrielDev <julian.alejandro.gabriel@gmail.com>
2025-04-14 07:04:15 -0600

    feat: update package.json and vite.config.ts for deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://julianngabrieldev.github.io/fm-advice-generator-app/'
 })
